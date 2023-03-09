16e627a518faf8773428612bacfdc7df4e20a9d9
davidgamboa114 <davidgamboa114@gmail.com>
2023-03-09 15:33:56 -0300

    fase2

---
+++ js/style.js
@@ -0,0 +1,15 @@
+// Seleccionar el elemento del título
+const titulo = document.getElementById('titulo');
+
+// Pedirle al usuario que ingrese su nombre
+const nombre = prompt('Por favor, ingresa tu nombre:');
+
+// Cambiar el texto del título
+titulo;
+titulo,
+textContent = `Bienvenido/a, ${nombre}!`;
+
+// Mostrar un mensaje de bienvenida
+
+alert
+alert(`¡Bienvenido/a, ${nombre}! Esperamos que disfrutes de nuestro sitio sobre Boca Juniors.`);
